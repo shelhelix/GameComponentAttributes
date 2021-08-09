27fe8bab10883bd9c099f1774e4b5017f2629cd0
alexandr.shelekhin <alexandr.shelekhin@gmail.com>
2021-08-09 20:04:49 +0700

    refactored NotNullOrEmpty attribute processing

---
+++ Assets/Plugins/GameComponentAttributes/Scripts/AttributesProcessor/NotNullOrEmptyProcessingHelpers/INotNullOrEmptyTypeProcessor.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+using GameComponentAttributes.Attributes;
+
+using Object = UnityEngine.Object;
+
+namespace GameComponentAttributes.AttributesProcessor.NotNullOrEmptyProcessingHelpers {
+	public interface INotNullOrEmptyTypeProcessor {
+		bool IsSuitableProcessor(object value);
+		void CheckObject(NotNullOrEmptyAttribute attribute, Object context, object value, FieldInfo fieldInfo);
+	}
+}
